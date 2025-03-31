401a10fed5ee0016a9675aa2cca7fcd4e19d5ad0
unknown <jacobvestergaard@hotmail.dk>
2025-03-31 19:28:48 +0200

    appwrite file, not working when trying to opload data, the response is 'No Appwrite project was specified. Please specify your project ID when initializing your Appwrite SDK.' I dont know why it doesn't work, I have set it up like in the video and other people have the problem on the internet

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -11,12 +11,21 @@ import  SearchBar  from '@/components/SearchBar';
 import { fetchMovies } from '@/services/api';
 import  useFetch from '@/services/useFetch'
 import MovieCard from '@/components/MovieCard';
+import { getTrendingMovies } from '@/services/appwrite';
 
 
 
 
 export default function HomeScreen() {
   const router = useRouter();
+
+  const {
+    data: trendingMovies,
+    loading: trendingLoading,
+    error: trendingError,
+  } = useFetch(getTrendingMovies);
+
+
 
   const {data: movies,
      loading: moviesLoading,
@@ -36,14 +45,14 @@ export default function HomeScreen() {
           <Image source={icons.logo} className="w-12 h-10 
             mt-20 mb-5 mx-auto"/>
 
-            {moviesLoading ? (
+            {moviesLoading || trendingLoading ? (
               <ActivityIndicator
                 size="large"
                 color="#0000ff"
                 className="mt-10 self-center"
                 />
-            ): moviesError ? (
-              <Text>Error: {moviesError?.message}</Text>
+            ): moviesError || trendingError ? (
+              <Text>Error: {moviesError?.message || trendingError?.message}</Text>
             ): (
                 <View className="flex-1 mt-5">
                   <SearchBar
@@ -51,8 +60,27 @@ export default function HomeScreen() {
                     placeholder="Search for a movie"
                     />
 
+                    {trendingMovies && (
+                      <View className="mt-10">
+                          <Text className="text-lg text-white font-bold mb-3">Trending Movies</Text>
+
+                        </View>
+                    )}
+
+
                     <>
                       <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
+
+                      <FlatList 
+                          className="mb-4 mt-3"
+                          data={trendingMovies} renderItem={({
+                          item, index }) => (
+                            <Text>{item.title}</Text>
+                          )}
+                          keyExtractor={(item) => item.movie_id.toString()}
+                        />
+
+
 
                       <FlatList
                           data={movies} 
--- services/appwrite.ts
+++ services/appwrite.ts
@@ -16,7 +16,7 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
            Query.equal('searchTerm',query)
            
        ])
-    
+
        if(result.documents.length > 0){
            const existingMovie = result.documents[0];
            await database.updateDocument(
@@ -27,19 +27,36 @@ export const updateSearchCount = async (query: string, movie: Movie) => {
                }
            )
        } else {
-           await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
-               searchTerm: query,
-               movie_id: movie.id,
-               count: 1,
-               title: movie.title,
-               poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-           })
+
+            try {
+                
+                await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+                    searchTerm: query,
+                    movie_id: movie.id,
+                    count: 1,
+                    title: movie.title,
+                    poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                })
+            } catch (error) {
+                console.log(error);
+            }
        }
    } catch (error) {
         console.log(error);
         throw error;
    }
-   
+}
 
+export const getTrendingMovies = async (): Promise<TrendingMovie[] | undefined> =>{
+    try {
+        const result = await database.listDocuments(DATABASE_ID,COLLECTION_ID,[
+            Query.limit(5),
+            Query.orderDesc('count'),
+        ])
 
+        return result.documents as unknown as TrendingMovie[];
+    } catch (error) {
+        console.log(error);
+        return undefined;
+    }
 }
